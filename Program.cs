9af146b6edd7286989a66703ef032c27ea6100b0
David Mordechai <david.mordechai79@gmail.com>
2022-04-10 22:51:00 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Minio;
+
+var minIo = new MinioClient()
+    .WithEndpoint(new Uri("localhost:9000"))
+    .WithCredentials("access-key", "secret-key")
+    .Build();
+
+var listArgs = new ListObjectsArgs()
+    .WithBucket("test");
+
+minIo
+    .ListObjectsAsync(listArgs)
+    .Subscribe(
+        item => { Console.WriteLine($"Object: {item.Key}"); },
+        ex => { Console.WriteLine($"OnError: {ex}"); },
+        () => { Console.WriteLine("Listed all objects in bucket 'test'"); }
+    );
+Console.ReadKey();
